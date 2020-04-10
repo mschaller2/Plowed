9bcd077dcad086b447d2f0d26b62404b7897d3b8
Andy <aking27@wisc.edu>
2020-04-10 15:52:07 -0500

    Add Payment Activity

---
+++ app/src/main/java/com/example/plowed/AddPaymentActivity.java
@@ -0,0 +1,14 @@
+package com.example.plowed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_payment);
+    }
+}
