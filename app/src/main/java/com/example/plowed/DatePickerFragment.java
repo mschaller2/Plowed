c62c5fa4f786efde502edff814b0d8f4e09ce1fe
Andy <aking27@wisc.edu>
2020-04-20 20:08:24 -0500

    After date is selected, displays to user

--- app/src/main/java/com/example/plowed/DatePickerFragment.java
+++ app/src/main/java/com/example/plowed/DatePickerFragment.java
@@ -8,9 +8,14 @@ import android.widget.DatePicker;
 
 import androidx.fragment.app.DialogFragment;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment {
+    int yearPicked;
+    int monthPicked;
+    int dayPicked;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
@@ -20,12 +25,19 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month, day);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
 
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the date chosen by the user
-    }
+//        Calendar c = Calendar.getInstance();
+//        c.set(Calendar.YEAR, year);
+//        c.set(Calendar.MONTH, month);
+//        c.set(Calendar.DAY_OF_MONTH, day);
+//        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+//        TextView textView = (TextView) findViewById(R.id.textView);
+//        textView.setText(currentDateString);
 
+    }
 }
--- app/src/main/java/com/example/plowed/RequestService.java
+++ app/src/main/java/com/example/plowed/RequestService.java
@@ -3,22 +3,53 @@ package com.example.plowed;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class RequestService extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class RequestService extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+    int year;
+    int month;
+    int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_service);
+
+        Button dateButton = (Button) findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
     }
 
     public void goToHandlePayment(View view){ startActivity(new Intent(this, HandlePayment.class)); }
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
+//    public void showDatePickerDialog(View v) {
+//        DialogFragment newFragment = new DatePickerFragment();
+//        newFragment.show(getSupportFragmentManager(), "datePicker");
+//    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView textView = (TextView) findViewById(R.id.dateText);
+        textView.setText(currentDateString);
     }
 }
