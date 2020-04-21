6c87902bcae7b4d7dabe6ab75e28b4c1e538c60e
Andy <aking27@wisc.edu>
2020-04-20 19:12:25 -0500

    Handle Payment google pay integration (non upi)

--- app/src/main/java/com/example/plowed/HandlePayment.java
+++ app/src/main/java/com/example/plowed/HandlePayment.java
@@ -1,58 +1,103 @@
 package com.example.plowed;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class HandlePayment extends AppCompatActivity {
-
-//    Uri uri =
-//            new Uri.Builder()
-//                    .scheme("upi")
-//                    .authority("pay")
-//                    .appendQueryParameter("pa", "your-merchant-vpa@xxx")       // virtual ID
-//                    .appendQueryParameter("pn", "your-merchant-name")          // name
-//                    .appendQueryParameter("mc", "your-merchant-code")          // optional
-//                    .appendQueryParameter("tr", "your-transaction-ref-id")     // optional
-//                    .appendQueryParameter("tn", "your-transaction-note")       // any note about payment
-//                    .appendQueryParameter("am", "your-order-amount")           // amount
-//                    .appendQueryParameter("cu", "INR")                         // currency
-//                    .appendQueryParameter("url", "your-transaction-url")  // optional
-//                    .build();
-
-
 //    EditText amount, note, name, upivirtualid;
 //    Button send;
 //    String TAG ="main";
 //    final int UPI_PAYMENT = 0;
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 991;
+    private PaymentsClient paymentsClient;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private static JSONObject baseConfigurationJson() throws JSONException {
+        return new JSONObject()
+                .put("apiVersion", 2)
+                .put("apiVersinMinor", 8)
+                .put("allowedPaymentMethods",
+                        new JSONArray().put(getCardPaymentMethod()));
+    }
+    private static JSONObject getGatewayTokenizationSpecification() throws JSONException {
+        return new JSONObject() {{      put("type", "PAYMENT_GATEWAY");
+            put("parameters", new JSONObject() {
+                {
+                    put("gateway", "example");
+                    put("gatewayMerchantId", "exampleGatewayMerchantId");
+                }
+            });
+        }};
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.handle_payment);
+        Wallet.WalletOptions walletOptions = new Wallet.WalletOptions.Builder().
+                setEnvironment(WalletConstants.ENVIRONMENT_TEST). //The test environment doesn't need an account -- production does
+                build();
 
-//        String GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
-//        int GOOGLE_PAY_REQUEST_CODE = 123;
-//
-//        Intent intent = new Intent(Intent.ACTION_VIEW);
-//        intent.setData(uri);
-//        intent.setPackage(GOOGLE_PAY_PACKAGE_NAME);
-//        activity.startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE);
+        paymentsClient = Wallet.getPaymentsClient(this, walletOptions);
+        paymentsClient = createPaymentsClient(this);
+        try {
+            isReadyToPay();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        Uri uri =
+//                new Uri.Builder()
+//                        .scheme("upi")
+//                        .authority("pay")
+//                        .appendQueryParameter("pa", "your-merchant-vpa@xxx")       // virtual ID
+//                        .appendQueryParameter("pn", "your-merchant-name")          // name
+//                        .appendQueryParameter("mc", "your-merchant-code")          // optional
+//                        .appendQueryParameter("tr", "your-transaction-ref-id")     // optional
+//                        .appendQueryParameter("tn", "your-transaction-note")       // any note about payment
+//                        .appendQueryParameter("am", "your-order-amount")           // amount
+//                        .appendQueryParameter("cu", "INR")                         // currency
+//                        .appendQueryParameter("url", "your-transaction-url")  // optional
+//                        .build();
 
-//        send = (Button) findViewById(R.id.send);
+//        send = (Button) findViewById(R.id.button);
 //        amount = (EditText)findViewById(R.id.amount_et);
 //        note = (EditText)findViewById(R.id.note);
 //        name = (EditText) findViewById(R.id.name);
@@ -75,8 +120,8 @@ public class HandlePayment extends AppCompatActivity {
 //                    Toast.makeText(HandlePayment.this," Amount is invalid", Toast.LENGTH_SHORT).show();
 //                }else{
 //
-////                    payUsingUpi(name.getText().toString(), upivirtualid.getText().toString(),
-////                            note.getText().toString(), amount.getText().toString());
+//                    payUsingUpi(name.getText().toString(), upivirtualid.getText().toString(),
+//                            note.getText().toString(), amount.getText().toString());
 //
 //                }
 //
@@ -84,6 +129,62 @@ public class HandlePayment extends AppCompatActivity {
 //            }
 //        });
     }
+
+    public static PaymentsClient createPaymentsClient(Activity activity) {
+        Wallet.WalletOptions walletOptions =
+                new Wallet.WalletOptions.Builder().setEnvironment(WalletConstants.ENVIRONMENT_TEST).build();
+        return Wallet.getPaymentsClient(activity, walletOptions);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void isReadyToPay() throws JSONException {
+        IsReadyToPayRequest readyToPayRequest = IsReadyToPayRequest.fromJson(baseConfigurationJson().toString());
+        Task<Boolean> task = paymentsClient.isReadyToPay(readyToPayRequest);
+        task.addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+            @Override
+            public void onComplete(@NonNull Task<Boolean> task) {
+                if(task.isSuccessful()){
+                    showGooglePlayButton(task.getResult());
+                } else {
+                    // handle the error accordingly
+                }
+            }
+        });
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        final String[] networks = new String[] {"VISA", "AMEX"};
+        final String[] authMethods = new String[] {"PAN_ONLY", "CRYPTOGRAM_3DS"};
+        JSONObject card = new JSONObject();
+        card.put("type", "CARD");
+        card.put("tokenizationSpecification", getGatewayTokenizationSpecification());
+        card.put("parameters", new JSONObject().put("allowedAuthMethods", new JSONArray(authMethods)).put("allowedCardNetworks", new JSONArray(networks)));
+        return card;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void loadPaymentData() throws JSONException {
+        final JSONObject paymentRequestJson = baseConfigurationJson();
+        paymentRequestJson.put("transactionInfo", new JSONObject()
+                    .put("totalPrice", "123.45")
+                    .put("totalPriceStatus", "FINAL")
+                    .put("currencyCode", "USD"));
+        paymentRequestJson.put("merchantInfo", new JSONObject()
+                    .put("merchantId", "0123456789")
+                    .put("merchantName", "Example Merchant"));
+
+        final PaymentDataRequest request = PaymentDataRequest.fromJson(paymentRequestJson.toString());
+        AutoResolveHelper.resolveTask(paymentsClient.loadPaymentData(request),this, LOAD_PAYMENT_DATA_REQUEST_CODE);
+    }
+
+    private void showGooglePlayButton(boolean userIsReadyToPay){
+        if(userIsReadyToPay){
+            //show google pay button
+        } else {
+            //don't show
+        }
+    }
+
 //    void payUsingUpi(  String name,String upiId, String note, String amount) {
 //        Log.e("main ", "name "+name +"--up--"+upiId+"--"+ note+"--"+amount);
 //        Uri uri = Uri.parse("upi://pay").buildUpon()
@@ -97,20 +198,25 @@ public class HandlePayment extends AppCompatActivity {
 //                .appendQueryParameter("cu", "INR")
 //                //.appendQueryParameter("refUrl", "blueapp")
 //                .build();
+//        String GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
+//        int GOOGLE_PAY_REQUEST_CODE = 123;
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(uri);
+//        intent.setPackage(GOOGLE_PAY_PACKAGE_NAME);
+//        activity.startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE);
 //
-//
-//        Intent upiPayIntent = new Intent(Intent.ACTION_VIEW);
-//        upiPayIntent.setData(uri);
-//
-//        // will always show a dialog to user to choose an app
-//        Intent chooser = Intent.createChooser(upiPayIntent, "Pay with");
-//
-//        // check if intent resolves
-//        if(null != chooser.resolveActivity(getPackageManager())) {
-//            startActivityForResult(chooser, UPI_PAYMENT);
-//        } else {
-//            Toast.makeText(HandlePayment.this,"No UPI app found, please install one to continue",Toast.LENGTH_SHORT).show();
-//        }
+////        Intent upiPayIntent = new Intent(Intent.ACTION_VIEW);
+////        upiPayIntent.setData(uri);
+////
+////        // will always show a dialog to user to choose an app
+////        Intent chooser = Intent.createChooser(upiPayIntent, "Pay with");
+////
+////        // check if intent resolves
+////        if(null != chooser.resolveActivity(getPackageManager())) {
+////            startActivityForResult(chooser, UPI_PAYMENT);
+////        } else {
+////            Toast.makeText(HandlePayment.this,"No UPI app found, please install one to continue",Toast.LENGTH_SHORT).show();
+////        }
 //
 //    }
 //
@@ -208,6 +314,6 @@ public class HandlePayment extends AppCompatActivity {
 //        }
 //        return false;
 //    }
-//
+
 
 }
