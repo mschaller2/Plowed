c7114fb248486f4dc491677878ed35fb257658f3
Andy <aking27@wisc.edu>
2020-04-28 17:21:48 -0500

    Driver rating activity created

---
+++ app/src/main/java/com/example/plowed/DriverReview.java
@@ -0,0 +1,37 @@
+package com.example.plowed;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DriverReview extends AppCompatActivity {
+    Button saveRating;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_review_activity);
+
+        saveRating = findViewById(R.id.saveRating);
+        ratingBar = findViewById(R.id.ratingBar);
+        saveRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rating = "Rating is :" + ratingBar.getRating() + "stars";
+                Toast.makeText(driver_review_activity.this, rating, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void getRecentDrivers(){
+
+    }
+    // based on recent drivers, user can rate them
+    // 1 to 5 star ratings
+    // User can add written rating
+}
--- app/src/main/java/com/example/plowed/Listings.java
+++ app/src/main/java/com/example/plowed/Listings.java
@@ -1,16 +1,25 @@
 package com.example.plowed;
 
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.Locale;
 
 public class Listings extends AppCompatActivity {
+    private FirebaseUser mUser;
+
+    TextView listings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listings_activity);
+
+        listings = (TextView) findViewById(R.id.listings);
     }
+
+
 
     // Need to dynamically update the listings on the activity
     // fetch from db
@@ -18,4 +27,15 @@ public class Listings extends AppCompatActivity {
 
     }
 
+    //Using this function from the user menu to dynamically adjust the listings text
+    private void userConfig(){
+        // may be empty
+        if (mUser.getDisplayName() != null){
+            listings.setText(String.format(Locale.ENGLISH, "Listings: %s",
+                    mUser.getDisplayName()));
+        }else{
+            listings.setText(R.string.welcome);
+        }
+
+    }
 }
