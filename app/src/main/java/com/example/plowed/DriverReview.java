a15ed80488af325f3566bb3b1c9d8b397e102610
Andy <aking27@wisc.edu>
2020-04-28 17:42:48 -0500

    Driver rating bug fixes

--- app/src/main/java/com/example/plowed/DriverReview.java
+++ app/src/main/java/com/example/plowed/DriverReview.java
@@ -3,6 +3,7 @@ package com.example.plowed;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -11,21 +12,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class DriverReview extends AppCompatActivity {
     Button saveRating;
     RatingBar ratingBar;
+    EditText writtenRating;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_review_activity);
 
-        saveRating = findViewById(R.id.saveRating);
-        ratingBar = findViewById(R.id.ratingBar);
-//        saveRating.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String rating = "Rating is :" + ratingBar.getRating() + "stars";
-//                Toast.makeText(DriverReview.this, rating, Toast.LENGTH_LONG).show();
-//            }
-//        });
+        saveRating = (Button) findViewById(R.id.saveRating);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        writtenRating = (EditText) findViewById(R.id.writtenReview);
+        saveRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rating = "Rating is: " + ratingBar.getRating() + " stars";
+                Toast.makeText(DriverReview.this, rating, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void getRecentDrivers(){
